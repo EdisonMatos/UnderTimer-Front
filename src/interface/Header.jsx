0ec999ad40724300065a434ce52d2db4d8cafa38
Edison Matos <edison.matos@live.com>
2025-06-20 07:39:59 -0300

    feat: Traz de volta header com botão de feedback

--- src/interface/Header.jsx
+++ src/interface/Header.jsx
@@ -9,7 +9,7 @@ export default function Header() {
       <p className="mt-0 text-xs text-center text-white opacity-50 lg:text-left">
         Beta - v0.8 (em desenvolvimento)
       </p>
-      <div className="flex justify-center w-full lg:justify-start">
+      <div className="flex justify-center w-full mb-10 lg:justify-start">
         <button className="p-2 mt-4 rounded-lg bg-primary w-fit hover:scale-105">
           <a href="https://forms.gle/Nmi7WzCzwqy3qP8Z6" target="_blank">
             Sugestões / Bugs / Reclamações
